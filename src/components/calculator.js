ebc85f3a255d27fb1fed25776ac9748a59967a23
ndorrh <ndorrh@gmail.com>
2022-10-26 21:20:02 +0100

    Add calculate function to Calculator component and make the App functional by defining a new state object

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,39 +1,62 @@
 import React, { Component } from 'react';
-// eslint-disable-next-line react/prefer-stateless-function
+import calculate from '../logic/calculate';
+
 class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+    this.but = this.but.bind(this);
+  }
+
+  but = (e) => {
+    const value = e.target.id;
+    const result = calculate(this.state, value);
+    this.setState(result);
+  }
+
   render() {
+    const { total, next, operation } = this.state;
     return (
-      <table className="cal-container">
+      <table className="cal-container" role="grid">
         <tbody>
-          <tr><td id="display" colSpan="4">0</td></tr>
           <tr>
-            <td id="AC">AC</td>
-            <td id="plus-minus">+/-</td>
-            <td id="percentage">%</td>
-            <td id="division" className="orange">÷</td>
+            <td id="display" colSpan="4">
+              {(total || '') + (operation || '') + (next || '') || 0}
+            </td>
+
           </tr>
           <tr>
-            <td id="7">7</td>
-            <td id="8">8</td>
-            <td id="9">9</td>
-            <td id="multiplication" className="orange">0</td>
+            <td id="AC" onClick={this.but} role="gridcell">AC</td>
+            <td id="+/-" onClick={this.but} role="gridcell">+/-</td>
+            <td id="%" onClick={this.but} role="gridcell">%</td>
+            <td id="÷" className="orange" role="gridcell" onClick={this.but}>÷</td>
           </tr>
           <tr>
-            <td id="4">4</td>
-            <td id="5">5</td>
-            <td id="6">6</td>
-            <td id="subtraction" className="orange">-</td>
+            <td id="7" onClick={this.but} role="gridcell">7</td>
+            <td id="8" onClick={this.but} role="gridcell">8</td>
+            <td id="9" onClick={this.but} role="gridcell">9</td>
+            <td id="x" className="orange" onClick={this.but} role="gridcell">x</td>
           </tr>
           <tr>
-            <td id="1">1</td>
-            <td id="2">2</td>
-            <td id="3">3</td>
-            <td id="plus" className="orange">+</td>
+            <td id="4" role="gridcell" onClick={this.but}>4</td>
+            <td id="5" role="gridcell" onClick={this.but}>5</td>
+            <td id="6" role="gridcell" onClick={this.but}>6</td>
+            <td id="-" className="orange" role="gridcell" onClick={this.but}>-</td>
           </tr>
           <tr>
-            <td id="0" colSpan="2">0</td>
-            <td id="fullStop">.</td>
-            <td id="equalTo" className="orange">=</td>
+            <td id="1" onClick={this.but} role="gridcell">1</td>
+            <td id="2" onClick={this.but} role="gridcell">2</td>
+            <td id="3" onClick={this.but} role="gridcell">3</td>
+            <td id="+" className="orange" onClick={this.but} role="gridcell">+</td>
+          </tr>
+          <tr>
+            <td id="0" colSpan="2" role="gridcell" onClick={this.but}>0</td>
+            <td id="." role="gridcell" onClick={this.but}>.</td>
+            <td id="=" className="orange" role="gridcell" onClick={this.but}>=</td>
           </tr>
         </tbody>
       </table>
