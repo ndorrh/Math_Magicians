a798fb0de0217defe574d190c6158bcc49f62a9f
ndorrh <ndorrh@gmail.com>
2022-10-31 21:20:25 +0100

    Add Header component and modify TodoContainer component

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TodosList } from './TodosList';
+import Header from './Header';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class TodoContainer extends React.Component {
@@ -31,6 +32,7 @@ class TodoContainer extends React.Component {
     const { todos } = this.state;
     return (
       <div>
+        <Header />
         <TodosList todos={todos} />
       </div>
     );
