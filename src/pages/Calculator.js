b05ae32138d405256bfa6f61bfec90e8239d8e88
ndorrh <ndorrh@gmail.com>
2022-11-02 13:42:21 +0100

    Add styling for Calculator page

--- src/pages/Calculator.js
+++ src/pages/Calculator.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Calculator from '../components/calculator';
+import styles from './Calculator.module.css';
 
 const CalculatorPage = () => (
-  <>
+  <div className={styles.container}>
     <h3>Let&apos;s do some maths!</h3>
     <Calculator />
-  </>
+  </div>
 );
 
 export default CalculatorPage;
