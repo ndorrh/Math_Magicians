e1d0bfc84e55c63583d2bfe531f4dd6251765796
ndorrh <ndorrh@gmail.com>
2022-11-02 13:10:45 +0100

    Modify Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
   <nav>
+    <h1>Math Magicians</h1>
     <ul>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/Calculator">Calculator</Link></li>
