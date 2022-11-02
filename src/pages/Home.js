72677af571d6712313ea896d6db5e68e94e8b30e
ndorrh <ndorrh@gmail.com>
2022-11-02 14:56:35 +0100

    Add styling for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,30 @@
 import React from 'react';
+import styles from './Home.module.css';
 
-function Home() {
-  return (
-    <>
-      <main>
-        <h3>Welcome to our page!</h3>
-        <p>
-          {' '}
-          Lorem ipsum dolor sit, amet consectetur adipisicing
-          elit.Nisi accusantium est quisquam eaque ipsum
-          perspiciatis veritatis repellendus?Explicabo iusto
-          nihil officia fugit delectus nobis, voluptate, quis
-          vero fuga veniam eum.Lorem ipsum dolor sit, amet conse
-          ctetur adipisicing elit.Nisi accusantium est quisquam eaque ipsum perspiciatis veritatis.
-        </p>
-        <p>
-          {' '}
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Nisi accusantium est quisquam eaque ipsum perspiciatis
-          veritatis repellendus?Explicabo iusto nihil officia
-          fugit delectus nobis, voluptate, quis vero fuga veniam eum.
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Nisi accusantium est quisquam eaque ipsum perspiciatis veritatis.
-        </p>
-      </main>
-    </>
-  );
-}
+const Home = () => (
+  <>
+    <main className={styles.main}>
+      <p className={styles.welcome}>Welcome to our page!</p>
+      <p className={styles.para}>
+        {' '}
+        Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit.Nisi accusantium est quisquam eaque ipsum
+        perspiciatis veritatis repellendus?Explicabo iusto
+        nihil officia fugit delectus nobis, voluptate, quis
+        vero fuga veniam eum.Lorem ipsum dolor sit, amet conse
+        ctetur adipisicing elit.Nisi accusantium est quisquam eaque ipsum perspiciatis veritatis.
+      </p>
+      <p className={styles.para}>
+        {' '}
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Nisi accusantium est quisquam eaque ipsum perspiciatis
+        veritatis repellendus?Explicabo iusto nihil officia
+        fugit delectus nobis, voluptate, quis vero fuga veniam eum.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Nisi accusantium est quisquam eaque ipsum perspiciatis veritatis.
+      </p>
+    </main>
+  </>
+);
 
 export default Home;
